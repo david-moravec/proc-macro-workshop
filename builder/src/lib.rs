a93701e92db358c49c486d44c3e7f5b5cd6eee69
David Moravec <moravec@numsolution.cz>
2024-06-17 08:43:25 +0200

    Do builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,101 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+fn type_is_option(ty: &syn::Type) -> bool {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = ty
+    {
+        if let Some(syn::PathSegment { ident, .. }) = segments.first() {
+            ident == "Option"
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
+
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!("Use only on Structs with named fields")
+    };
+
+    let fields_opt = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! { #name: std::option::Option<#ty> }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let build_attrs = fields.iter().map(|f| {
+        let name = &f.ident;
+
+        if type_is_option(&f.ty) {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
+        }
+    });
+
+    let expanded = quote! {
+        pub struct #bident {
+            #(#fields_opt,)*
+        }
+
+        impl #bident {
+            #(#methods)*
+
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(
+                    #name {
+                        #(#build_attrs,)*
+                    }
+                )
+            }
+        }
+
+        impl #name {
+            pub fn builder() -> #bident {
+                #bident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None
+                }
+            }
+        }
+
+    };
+
+    TokenStream::from(expanded)
 }
