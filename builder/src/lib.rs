176da5968d235ae957750a5a9c2871187e409e31
David Moravec <moravec@numsolution.cz>
2024-06-25 13:05:40 +0200

    do 08

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use std::fmt;
+use syn::{parse_macro_input, DeriveInput, Error};
 
 fn inner_type<'a, 'b>(ty: &'a syn::Type, wrapper_type: &'b str) -> Option<&'a syn::Type> {
     if let syn::Type::Path(syn::TypePath {
@@ -34,17 +35,52 @@ fn inner_type<'a, 'b>(ty: &'a syn::Type, wrapper_type: &'b str) -> Option<&'a sy
     }
 }
 
-fn each_attribute_name_literal(attrs: Vec<syn::Attribute>) -> Option<syn::Ident> {
+fn make_error<T: quote::ToTokens, U: fmt::Display>(tokens: T, message: U) -> syn::Error {
+    Error::new_spanned(tokens, message)
+}
+
+fn each_attribute_name_literal(attrs: Vec<syn::Attribute>) -> Option<Result<syn::Ident, Error>> {
     for attr in attrs.into_iter() {
         if attr.path().is_ident("builder") {
-            if let syn::Meta::List(syn::MetaList { tokens, .. }) = attr.meta {
-                for t_tree in tokens.into_iter() {
-                    if let proc_macro2::TokenTree::Literal(literal) = t_tree {
-                        return Some(syn::Ident::new(
-                            literal.to_string().trim_matches('"'),
-                            proc_macro2::Span::call_site(),
-                        ));
+            if let syn::Meta::List(syn::MetaList { ref tokens, .. }) = attr.meta {
+                let mut t_tree_iter = tokens.clone().into_iter();
+
+                match t_tree_iter.next().unwrap() {
+                    proc_macro2::TokenTree::Ident(ident) => {
+                        if ident != proc_macro2::Ident::new("each", ident.span()) {
+                            return Some(Err(make_error(
+                                attr.meta,
+                                r#"expected `builder(each = "...")`"#,
+                            )));
+                        }
                     }
+                    tt => {
+                        return Some(Err(Error::new(
+                            tt.span(),
+                            format!("Expected 'each', found {}", tt),
+                        )));
+                    }
+                };
+
+                match t_tree_iter.next().unwrap() {
+                    proc_macro2::TokenTree::Punct(punct) => {
+                        if punct.as_char() != '=' {
+                            return Some(Err(Error::new(punct.span(), "Expected '='")));
+                        }
+                    }
+                    tt => {
+                        return Some(Err(Error::new(
+                            tt.span(),
+                            format!("Expected '=', found {}", tt),
+                        )));
+                    }
+                };
+
+                if let proc_macro2::TokenTree::Literal(literal) = t_tree_iter.next().unwrap() {
+                    return Some(Ok(syn::Ident::new(
+                        literal.to_string().trim_matches('"'),
+                        proc_macro2::Span::call_site(),
+                    )));
                 }
             }
         }
@@ -95,15 +131,20 @@ pub fn derive(input: TokenStream) -> TokenStream {
         } else {
             if let Some(each_name) = each_attribute_name_literal(f.attrs.clone()) {
                 let inner_ty = inner_type(ty, "Vec").expect("Each attribute must be Vec<T>");
-                quote! {
-                        pub fn #each_name(&mut self, #each_name: #inner_ty) -> &mut Self {
-                            if let Some(ref mut v) = self.#name {
-                                v.push(#each_name);
-                            } else {
-                                self.#name = Some(vec![#each_name]);
-                            }
-                            self
+                match each_name {
+                    Ok(each_name) => {
+                        quote! {
+                                pub fn #each_name(&mut self, #each_name: #inner_ty) -> &mut Self {
+                                    if let Some(ref mut v) = self.#name {
+                                        v.push(#each_name);
+                                    } else {
+                                        self.#name = Some(vec![#each_name]);
+                                    }
+                                    self
+                                }
                         }
+                    }
+                    Err(err) => err.to_compile_error(),
                 }
             } else {
                 quote! {
